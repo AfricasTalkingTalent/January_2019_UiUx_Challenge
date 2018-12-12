26639f058f91c98069c5f45fceab5f990cf4b7a3
Ogutu Brian <codingbrian58@gmail.com>
2018-12-12 23:15:02 +0300

    adds migrations of the users table and registers the Users application

--- AutheticationProject/User/models.py
+++ AutheticationProject/User/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=250, blank=True, null=True)
+    email_address = models.CharField(max_length=250,blank=True,null=True)
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
