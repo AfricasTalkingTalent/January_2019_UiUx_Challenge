6510b2cd686bd5f04aa0bb38691af41e2de148b7
domanisamuel <wafulasammie@gmail.com>
2018-12-13 18:59:12 +0300

    updated VERIFCATION

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port = process.env.port || 3000; //define port
 var bodyParser = require('body-parser');
 
 const options = {
-    apiKey: '29d37ee7b679d71bf33600efc9272ad278f7b42d7cd45ee5943bcbe283874d73',  // use your sandbox app API key for development in the test environment
+    apiKey: '47559095d486028e154d6fb8739c88a9b07275b54b35080b9020d439d67ba670',  // use your sandbox app API key for development in the test environment
     username: 'samuelbarasa',      // use 'sandbox' for development in the test environment
 };
 const AfricasTalking = require('africastalking')(options);
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/views/verification.html"));
 app.get('/verify', function(req, res) {
     res.sendFile(__dirname + "/views/verification.html");
-  });
+});
 
 //create random verification code
 function randomInRange(from, to) {
@@ -51,6 +51,15 @@ app.post('/', function(request, response){
             response.sendStatus(401);
         });
     response.redirect('/verify');
+});
+app.post('/verify', function(request, response){
+    var verificationCode = req.body.verification;
+    if (verificationCode === randomInRange()) {
+        response.sendStatus(200);
+        response.redirect('/');
+    }else {
+        response.sendStatus(500);
+    }
 });
 
 app.listen(port, () => {
