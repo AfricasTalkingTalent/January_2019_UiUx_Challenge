ae05dd1b4b904c5666aaa9b067ab2b1d61256ccd
ottagit <ottacc66@gmail.com>
2018-12-13 17:25:18 +0300

    bootstrap user signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'static_pages/home'
+  get '/signup', to: 'users#new'
 end
