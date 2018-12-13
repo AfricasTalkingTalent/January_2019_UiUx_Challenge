b6074687c0a25ee0ef74babca7dba68a69a5c23b
Ian Odhiambo <you@example.com>
2018-12-13 17:35:12 +0300

    working sending token

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,16 +8,21 @@ class RegistrationForm(FlaskForm):
     username = StringField('Enter your username', validators=[Required()])
     phone_no = StringField('Your phone number',
                            validators=[Required()])
-    password = PasswordField('Password', validators=[Required(), EqualTo(
-        'password_confirm', message='Passwords must match')])
-    password_confirm = PasswordField(
-        'Confirm Passwords', validators=[Required()])
+    # password = PasswordField('Password', validators=[Required(), EqualTo(
+    #     'password_confirm', message='Passwords must match')])
+    # password_confirm = PasswordField(
+    #     'Confirm Passwords', validators=[Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self, data_field):
-        if User.query.filter_by(phone_no=data_field.data).first():
-            raise ValidationError('There is an account with that phone number')
+    # def validate_email(self, data_field):
+    #     if User.query.filter_by(phone_no=data_field.data).first():
+    #         raise ValidationError('There is an account with that phone number')
 
-    def validate_username(self, data_field):
-        if User.query.filter_by(username=data_field.data).first():
-            raise ValidationError('That username is taken')
+    # def validate_username(self, data_field):
+    #     if User.query.filter_by(username=data_field.data).first():
+    #         raise ValidationError('That username is taken')
+
+
+class TokenForm(FlaskForm):
+    token = StringField('Please enter security token?',
+                        validators=[Required()])
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session, Response
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, TokenForm
 from .. import db
-from random import choices
+import random
+import string
 
 from . import main
 
@@ -10,20 +11,61 @@ from . import main
 from ..request import send_sms
 
 
+def generate_token():
+    '''
+    generate random alphanumeric token upto 9 chars
+    '''
+    token = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for i in range(9))
+    return token
+
+
+def send_token(name, phone_number, token):
+    '''
+    sends token to phone
+    '''
+    message = f"Thank {name} please confirm account with {token}."
+
+    responds = send_sms(message, phone_number)
+    print(responds)
+    return responds
+
+
 @main.route('/register', methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    # if form.validate_on_submit():
+    if form.validate_on_submit():
+        phone_number = form.phone_no.data
+        username = form.username.data
+        # password = form.password.data
 
+        token = generate_token()
+        send_token(username, phone_number, token)
+
+        # session['phone_number'] = phone_number
+        # session['username'] = username
+        # session['password'] = password
+        # session['token'] = token
+        print(phone_number)
+        return redirect(url_for("main.token"))
     return render_template('register.html', reg_form=form)
+
+
+@main.route('/token', methods=["GET", 'POST'])
+def token():
+    tform = TokenForm()
+    if tform.validate_on_submit():
+        token = tform.token.data
+        # phone_number = session('phone_number')
+        # username = session('username')
+        # password = session('password')
+        # token_ori = session('token')
+        if token == token_ori:
+            print('great')
+    return render_template('token.html', tform=tform)
 
 
 @main.route('/')
 def index():
 
-    message = 'This is a tryx'
-    number = '+254712725144'
-
-    responds = send_sms(message, number)
-    print(responds)
-    return render_template('index.html', responds=responds)
+    return render_template('index.html')
