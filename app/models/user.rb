3a570a4a5caa90b30511c318b339c7e4dafe6bdd
ottagit <ottacc66@gmail.com>
2018-12-13 18:09:17 +0300

    validate user form details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true },
+    length: { minimum: 12 }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,30 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "phone number should be present" do
+    @user.phone_number = "    "
+    assert_not @user.valid?
+  end
+
+  test "phone number should consist of integers only" do
+    @user.phone_number = "254string2"
+    assert_not @user.valid?
+  end
+
+  test "phone number should have at least 12 digits" do
+    @user.phone_number = "25470670500"
+    assert_not @user.valid?
+  end
+
+  test "phone number should be unique" do
+    user_clone = @user.dup
+    @user.save
+    assert_not user_clone.valid?
+  end
 end
