0e1c0394d6749a38f5e93aae95a5f6af27a5b4f1
domanisamuel <wafulasammie@gmail.com>
2018-12-13 16:40:41 +0300

    was able to create and send confirmation code to user's phone number

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var app = express(); //create instance of express app
 var port = process.env.port || 3000; //define port
 var bodyParser = require('body-parser');
 
+const options = {
+    apiKey: '85e656dc062f647943301407869f4289d5955b10590b9417b5932dbbcfab53e9',  // use your sandbox app API key for development in the test environment
+    username: 'samuelbarasa',      // use 'sandbox' for development in the test environment
+};
+const AfricasTalking = require('africastalking')(options);
+
+// Initialize a service e.g. SMS
+sms = AfricasTalking.SMS
+
 //get path
 var path = require('path')
 
@@ -13,16 +22,23 @@ app.use(express.static('views'));
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-//create an empty array that will store the data after submission
-let details = [];
-
 app.post('/', function(request, response){
-    details.push(request.body.username);
-    details.push(request.body.phone);
-    details.push(request.body.password);
-    console.log(details) 
-    response.end();   
-});
+    // Use the service
+    const moses = {
+        to:  request.body.phone,
+        message: "I'm a lumberjack and its ok, I work all night and sleep all day"
+    }
+    // Send message and capture the response or error
+    sms.send(moses)
+        .then( res => {
+            console.log(res);
+            response.sendStatus(200);
+        })
+        .catch( error => {
+            console.log(error);
+            response.sendStatus(401);
+            });
+        });
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
