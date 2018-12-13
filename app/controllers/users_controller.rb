16bd7028685b3a81747f1e59ebf5a50352601a6d
ottagit <ottacc66@gmail.com>
2018-12-13 19:24:56 +0300

    create user signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful signup
+    else
+      render 'new'
+    end
+  end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :phone_number, :passowrd, :password_confirmation)
+    end
 end
